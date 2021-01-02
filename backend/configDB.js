c82b499d1f4432651c4849dcab12eb0d82cfb575
Ayushmaan tiwari <41547232+iamcaptaain@users.noreply.github.com>
2021-01-02 15:27:35 +0530

    Update configDB.js

--- backend/configDB.js
+++ backend/configDB.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 // const mongoDb_password = process.env.DB_password;
 
-const uri = `mongodb+srv://Vishwajeet:Vishwajeet@123@cluster0.inh1w.mongodb.net/signup?retryWrites=true&w=majority`;
+const uri = "your key should here ";
 
 mongoose
   .connect(uri, {
